9d35a342507d9f8b9d841a5586862696a285a976
unknown <adrousth@sbcglobal.net>
2022-07-14 17:03:47 -0500

    adding html/javascript

--- dao/request_dao.py
+++ dao/request_dao.py
@@ -1,4 +1,5 @@
 import psycopg
+from model.request import Request
 
 
 class RequestDao:
@@ -10,5 +11,20 @@ class RequestDao:
             with conn.cursor() as cur:
                 cur.execute("SELECT * FROM ers_reimbursements"
                             " WHERE status = %s;", (status,))
-                requests = cur.fetchall()
+                requests = []
+                for request in cur:
+                    requests.append(Request(request[0], request[1], request[2], request[3], request[4], request[5],
+                                            request[6], request[7], request[8], request[9]).to_dict())
         return requests
+
+
+    def get_all_requests(self):
+        with psycopg.connect(self.__connection_string) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT * FROM ers_reimbursements")
+                requests = []
+                for request in cur:
+                    requests.append(Request(request[0], request[1], request[2], request[3], request[4], request[5],
+                                            request[6], request[7], request[8], request[9]).to_dict())
+        return requests
+
--- service/user_service.py
+++ service/user_service.py
@@ -1,3 +1,4 @@
+from model.user import User
 from dao.user_dao import UserDao
 
 
@@ -7,6 +8,7 @@ class UserService:
 
     def get_user_by_username(self, username):
         user = self.user_dao.get_user_by_username(username)
+        return User(user[0], user[1], user[2], user[3], user[4], user[5])
 
 
 
